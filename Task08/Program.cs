5f80fdc16bd4a5971c6c06b86433445887e18ccb
dvsni <danil.shir@outlook.com>
2022-10-03 23:44:11 +0300

    Решили восьмую задачу

---
+++ Task08/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <=1)
+{
+    Console.WriteLine("Введенное число меньше 2, поэтому четных чисел от 1 до N не существует");
+}
+else
+{
+    Console.WriteLine($"Четные числа, которые есть от 1 до {number}: ");
+
+    for (int i = 2; i <= number; i += 2)
+    {
+        Console.WriteLine(i);
+    }
+}
