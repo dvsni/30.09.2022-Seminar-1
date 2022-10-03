5e681724c137541b6f2108c9f13098ebe6d7b313
dvsni <danil.shir@outlook.com>
2022-10-03 20:48:56 +0300

    Дополнили пятую задачу

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -6,6 +6,7 @@
 
 Console.WriteLine("Введите целое число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Все целые числа в промежутке от {-number} до {number}: ");
 
 int counter = -number;
 while (counter <= number )
