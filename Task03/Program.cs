811275c318e77e40da5e201e159a39f892ab7444
dvsni <danil.shir@outlook.com>
2022-10-03 20:45:32 +0300

    Решили третью задачу

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -5,30 +5,30 @@
 
 Console.WriteLine("Введите номер недели: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
-switch(number1)
+switch (number1)
 {
     case 1:
-    Console.WriteLine("Понедельник");
-    break;
+        Console.WriteLine("Понедельник");
+        break;
     case 2:
-    Console.WriteLine("Вторник");
-    break;
+        Console.WriteLine("Вторник");
+        break;
     case 3:
-    Console.WriteLine("Среда");
-    break;
+        Console.WriteLine("Среда");
+        break;
     case 4:
-    Console.WriteLine("Четверг");
-    break;
+        Console.WriteLine("Четверг");
+        break;
     case 5:
-    Console.WriteLine("Пятница");
-    break;
+        Console.WriteLine("Пятница");
+        break;
     case 6:
-    Console.WriteLine("Суббота");
-    break;
+        Console.WriteLine("Суббота");
+        break;
     case 7:
-    Console.WriteLine("Воскресенье");
-    break;
+        Console.WriteLine("Воскресенье");
+        break;
     default:
-    Console.WriteLine("Нет такого дня недели");
-    break;
+        Console.WriteLine("Нет такого дня недели");
+        break;
 }
