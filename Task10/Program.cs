70a8e2592409edd7fc92d0fcce841d4b3a874746
dvsni <danil.shir@outlook.com>
2022-10-05 13:58:52 +0300

    task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -5,7 +5,17 @@
 // 782 -> 8
 // 918 -> 1
 
+// 1 метод:
 Console.WriteLine("Введите трехзначное число: ");
 int firstDigit1 = Convert.ToInt32(Console.ReadLine());
 string number1 = Convert.ToString(firstDigit1);
 Console.WriteLine("Вторая цифра числа -> "+number1[1]);
+
+// 2 метод:
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digitIndex = 1;
+int digit = number.ToString()[digitIndex] - '0';
+Console.WriteLine($"Вторая цифра числа-> {digit}");
+
+
