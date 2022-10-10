95066d197c600291ceb7ce3aca631ba45f3130c7
dvsni <danil.shir@outlook.com>
2022-10-10 14:26:42 +0300

    task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -5,17 +5,31 @@
 // 782 -> 8
 // 918 -> 1
 
-// 1 метод:
-Console.WriteLine("Введите трехзначное число: ");
-int firstDigit1 = Convert.ToInt32(Console.ReadLine());
-string number1 = Convert.ToString(firstDigit1);
-Console.WriteLine("Вторая цифра числа -> "+number1[1]);
+// // 1 метод:
+// Console.WriteLine("Введите трехзначное число: ");
+// int firstDigit1 = Convert.ToInt32(Console.ReadLine());
+// string number1 = Convert.ToString(firstDigit1);
+// Console.WriteLine("Вторая цифра числа -> "+number1[1]);
 
-// 2 метод:
-Console.WriteLine("Введите трехзначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int digitIndex = 1;
-int digit = number.ToString()[digitIndex] - '0';
-Console.WriteLine($"Вторая цифра числа-> {digit}");
+// // 2 метод:
+// Console.WriteLine("Введите трехзначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int digitIndex = 1;
+// int digit = number.ToString()[digitIndex] - '0';
+// Console.WriteLine($"Вторая цифра числа-> {digit}");
 
+//3 метод:
 
+//4-й метод:
+// Console.WriteLine ("Введите трехзначное число");
+// int number = int.Parse(Console.ReadLine());
+// Console.WriteLine ($"Вторая цифра в числе-> {number/10%10}");
+
+Console.WriteLine("Введите трёхзначное число: "); 
+int number = Convert.ToInt32(Console.ReadLine()); 
+ 
+int firstDigit = number/100; 
+int twoDigitsNumber = number - 100*firstDigit; 
+int secondDigit = twoDigitsNumber/10; 
+ 
+Console.WriteLine($"Вторая цифра в числе-> {secondDigit}");
