a188b51b1ae57c2f33d9d28c5dcefe50bf0a4dfc
dvsni <danil.shir@outlook.com>
2022-10-12 01:34:06 +0300

    Task24

---
+++ Task24/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая
+// принимает на вход число (А)
+// и выдает сумму чисел от 1 до А
+// 7->28
+// 4->10
+// 8->36
+
+Console.WriteLine("Введите число: ");
+string str = Console.ReadLine()
+int number = Converter.ToInt32(Console.ReadLine);
+
