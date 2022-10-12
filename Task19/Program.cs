75848a03c966f20e491c2b8cd7067bbaf296686a
dvsni <danil.shir@outlook.com>
2022-10-13 00:47:23 +0300

    task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -5,6 +5,8 @@
 // 12821 -> да
 // 23432 -> да
 
+//1-ый способ:
+
 Console.WriteLine("Введите пятизначное число: ");
 string? number = Console.ReadLine();
 
@@ -22,3 +24,52 @@ if(number!.Length == 5)
 }
 else
 Console.WriteLine($"Введите верное число: ");
+
+
+//2-ой способ:
+
+// int[] digits = new int[100];
+// string? numberstring;
+// int miss = 0;
+
+// Console.WriteLine("Введите число, чтобы проверить его на палидромность:");
+// numberstring = Console.ReadLine();
+// int size = numberstring.Length;
+// for (int i = 0; i < size; i++)
+// {
+//     digits[i] = getdigit(numberstring, i + 1);
+// }
+
+// for (int i = 0; i < (size / 2); i++)
+// {
+//     if ((digits[i]) != (digits[size - i-1]))
+//     {
+//         miss++;
+//     }
+// }
+
+// if (miss > 0)
+// {
+//     Console.WriteLine("Не является палиндром");
+// }
+// else
+// {
+//     Console.WriteLine("Палиндром");
+// }
+
+
+// int getdigit(string numba, int count) 
+// {
+//     int sizenumber = numberstring.Length; 
+//     int intnumber = Convert.ToInt32(numba);
+//     int doublei = 0;
+//     int Digit1 = default;
+//     while (doublei < count)
+//     {
+//         Digit1 = intnumber / Convert.ToInt32(Math.Pow(10, (sizenumber - 1)));         
+//         intnumber = intnumber - Digit1 * Convert.ToInt32(Math.Pow(10, (sizenumber - 1)));
+//         doublei++;
+//         sizenumber--;
+//     }
+//     return Digit1;
+// }
