c3729e2b6f3cba404a91e260155f1542aeea2385
dvsni <danil.shir@outlook.com>
2022-10-15 00:14:26 +0300

    task29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -3,26 +3,31 @@
 // и выводит их на экран.
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
-Console.WriteLine("Вводим массим из 8 чисел :");
-
-int[] FillArray(int number)
-{
-    int[] array = new int[number];
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = new Random().Next(1, 41);
-    }
-    return array;
-}
-void PrintArray(int[] array)
-{
-    Console.Write($"[{array[0]}");
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($", {array[i]}");
-    }
-    Console.Write($"]");
-}
-
-int [] myArray = FillArray(8);
+Console.WriteLine("Введите размерность массива:"); 
+int number = Convert.ToInt32(Console.ReadLine()); 
+int[] fillarray = new int[number]; 
+ 
+int[] FillArray(int[] array, int num) 
+{ 
+     
+    for (int i = 0; i < array.Length; i++) 
+    { 
+        array[i] = new Random().Next(1, 41); 
+    } 
+    return array; 
+} 
+void PrintArray(int[] array2) 
+{ 
+   Console.Write($"[");    
+   for (int i = 0; i < array2.Length; i++) 
+    { 
+        Console.Write($"{array2[i]}"); 
+    if (i < array2.Length - 1) 
+ 
+        Console.Write(", "); 
+} 
+Console.Write($"]"); 
+} 
+ 
+int [] myArray = FillArray(fillarray, number); 
 PrintArray(myArray);
