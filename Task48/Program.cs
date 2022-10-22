742693f38792115dca55958788a02c2b56190bc5
dvsni <danil.shir@outlook.com>
2022-10-22 13:28:20 +0300

    task48

---
+++ Task48/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте двумерный массив рамзера m на n,
+// каждый элемент в массиве находится по
+// формуле: Amn = m+n.
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0123
+// 1234
+// 2345
+
+int[,] CreateAMatrixRndInt(int rows, int columns)
+{
+    var matrix = new int[rows, columns];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+        if(j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, ");
+        else Console.Write($"{matrix[i, j], 4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] array2D = CreateAMatrixRndInt(3, 4);
+PrintMatrix(array2D);
