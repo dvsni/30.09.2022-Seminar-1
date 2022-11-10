e0c1c09fdec6a45d54f32d687de0e088b9ee41b2
dvsni <danil.shir@outlook.com>
2022-11-11 02:20:15 +0300

    task57

---
+++ Task57/Program.cs
@@ -0,0 +1,65 @@
+﻿// Составить частотный словарь элементов
+// двумерного массива. Частотный словарь
+// содержит информацию о том, сколько раз встречается элемент
+// входных данных.
+
+int[,] CreateAMatrixRndInt(int rows, int columns, int min, int max)
+{
+    var matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+        if(j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, ");
+        else Console.Write($"{matrix[i, j], 4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[] NewArray(int[,] matrix)
+{
+    int[] newArray = new int[matrix.Length];
+    int k = 0; 
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            newArray[k] = matrix[i, j];
+            k++;
+        }
+    }
+    return newArray;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}; ");
+        else Console.Write($"{arr[i]}. ");
+    }
+}
+
+int[,] array2D = CreateAMatrixRndInt(4, 4, 1, 10);
+PrintMatrix(array2D);
+Console.WriteLine("");
+int[] array = NewArray(array2D);
+Array.Sort(array);
+PrintArray(array);
+
