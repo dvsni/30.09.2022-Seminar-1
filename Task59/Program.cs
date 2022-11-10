77d23e3568a16e64b337f17be54172802b7dd2e4
dvsni <danil.shir@outlook.com>
2022-11-11 02:36:53 +0300

    task59

---
+++ Task59/Program.cs
@@ -0,0 +1,78 @@
+﻿// Задайте двумерный массив из целых чисел.
+// Напишите программу, которая удалит строку и столбец, на
+// пересечении которых расположен наименьший элемент массива.
+// Например задан массив:
+// 1472
+// 5923
+// 8424
+// 5267
+// Наименьший элемент -1, на выходе получим
+// следующий массив:
+// 923
+// 424
+// 267
+
+int[,] CreateAMatrixRndInt(int rows, int columns, int min, int max)
+{
+    var matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+        if(j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, ");
+        else Console.Write($"{matrix[i, j], 4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[] FindMin(int[,] matrix)
+{
+    int min = matrix[0, 0];
+    int minRow = 0;
+    int minColumn = 0;
+    for (int i = 1; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 1; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] < min)
+            {
+                min = matrix[i, j];
+                minRow = i;
+                minColumn = j;
+
+            }
+        }
+    }
+    return new int[] {min, minRow, minColumn};
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}; ");
+        else Console.Write($"{arr[i]}. ");
+    }
+}
+
+int[,] array2D = CreateAMatrixRndInt(4, 4, 1, 10);
+PrintMatrix(array2D);
+
+int[] min = FindMin(array2D);
+PrintArray(min);
